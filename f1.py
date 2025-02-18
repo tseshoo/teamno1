ca5304eb632e6dbd6227d8562cf21ac862bf353b
User <user@Users-MacBook-Air-3.local>
2025-02-18 12:11:11 +0800

    Add new files f1.py and qodana.yaml, and update main.py

---
+++ f1.py
@@ -0,0 +1,14 @@
+def nemeh(a: float, b: float) -> float:
+    return a + b
+
+
+def hasah(a: float, b: float) -> float:
+    return a - b
+
+
+if __name__ == "__main__":
+    num1 = float(input("1: "))
+    num2 = float(input("2: "))
+
+    print(f"niilber: {nemeh(num1, num2)}")
+    print(f"ylgaa: {hasah(num1, num2)}")
